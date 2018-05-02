5a5c6aa0fcb37e44617e9871947cee8d7c9407f5
zzes <kuanglong0312@sina.com>
2018-05-02 17:14:34 +0800

    user表

---
+++ app/model/user.js
@@ -0,0 +1,44 @@
+'use strict'
+
+module.exports = app => {
+  const {INTEGER, STRING, DATE} = app.Sequelize
+  
+  const User = app.model.define('user', {
+    username: {
+      type: STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        is: /^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){1,50}$/i,
+        isLowercase: true,
+      }
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+        isLowercase: true
+      }
+    },
+    livecode: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+        is: /^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){1,50}$/i,
+        isLowercase: true,
+      }
+    }
+  }, {
+    timestamps: false,
+    tableName: 'user',
+    underscored:false
+  })
+
+  return User
+}
