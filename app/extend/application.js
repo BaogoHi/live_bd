87ffedc8f4d7d880b359fb7a4e8c339c19526384
zzes <kuanglong0312@sina.com>
2018-05-02 17:15:28 +0800

    application延生方法

---
+++ app/extend/application.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = {
+  generateJWT(id, username) {
+    const { config } = this
+    const token = this.jwt.sign({ id, username }, config.jwt.secret)
+    return token
+  },
+  verifyToken(ctx) {
+    const {config} = this
+    const token = config.jwt.getToken(ctx)
+    if(!token) return null
+    return this.jwt.verify(token, config.jwt.secret)
+  },
+  getUserJson(user, ctx, check) {
+    user = user.get()
+    const {config} = this
+    let token = config.jwt.getToken(ctx)
+    if(!token) {
+      token = 'Bearer ' + this.generateJWT(user.id, user.username)
+    }
+    if(check === 1){
+      return {token}
+    } else if(check === 0) {
+      return {
+        username: user.username,
+        email: user.email
+      }
+    } else {
+      return {error:'联系管理员吧'}
+    } 
+  }
+}
