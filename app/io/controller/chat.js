8aa15a550668dfd738d45cbfbcd8d77af6d6111d
zzes <kuanglong0312@sina.com>
2018-05-03 15:16:22 +0800

    websocket聊天室功能

---
+++ app/io/controller/chat.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class ChatController extends Controller {
+  /**
+   * 接收信息,并推送
+   */
+  async newMessage() {
+    const {ctx, app} = this
+    const {socket, logger} = ctx
+    const nsp = app.io.of('/')
+    let {room, username} = socket.handshake.query
+    const message = ctx.args[0] || {}
+    logger.info(`new message `,message)
+    try {
+      let data = {
+        message: message.data,
+        username: username,
+        time: message.time
+      }
+      app.io.to(room).emit('new message', data)
+      app.redis.lpush('message', JSON.stringify(data))
+      app.redis.expire('message', 10*60)
+    } catch (error) {
+      logger.error(error)
+    }
+  }
+  /**
+   * redis 中查询历史数据，并推送
+   */
+  async oldMessage() {
+    const {ctx, app} = this
+    const {socket, logger} = ctx
+    const nsp = app.io.of('/')
+    let {room} = socket.handshake.query
+    const result = await app.redis.lrange('message', 0, 10)
+    if(result.length>0) { 
+      nsp.to(room).emit('old message', result)
+    }else {}
+  }
+}
+
+module.exports = ChatController;
---
+++ app/io/middleware/connection.js
@@ -0,0 +1,27 @@
+'use strict'
+
+module.exports = app => {
+  return async (ctx, next) => {
+    const nsp = app.io.of('/')
+    const {socket,logger} = ctx
+    const { room, username } = socket.handshake.query
+    socket.join(room)
+    logger.info(`new user come on: ${username}, ${room}`)
+    // 用户加入
+    nsp.adapter.clients([room], (err, clients) => {
+      logger.info(clients)
+      nsp.to(room).emit('online', {
+        clientsNum: clients.length,
+        newClient: `欢迎${username}加入房间` 
+      })
+    })
+    await next()
+    // 用户退出
+    nsp.adapter.clients([room], (err, clients) => {
+      nsp.to(room).emit('online', {
+        clientsNum:clients.length,
+        outClient: `${username}退出房间`
+      })
+    })
+  }
+}
