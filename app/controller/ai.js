8d0d1b14edb88b1472c87c02e2a632c1150d5479
zzes <kuanglong0312@sina.com>
2018-05-11 17:18:59 +0800

    智能机器人回复

---
+++ app/controller/ai.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class AiController extends Controller {
+  async chat() {
+    const {ctx,app} = this
+    const {username} = app.verifyToken(ctx)
+    const data = ctx.request.body.question
+    console.log(data)
+    const answer = await ctx.curl(`http://api.ruyi.ai/v1/message`, {
+      method: 'POST',
+      contentType: 'json',
+      dataType: 'json',
+      data: {
+        app_key: app.config.aiKey,
+        user_id: username,
+        q: data
+      }
+    })
+    ctx.body = answer.data.result.intents[0].result.text
+  }
+}
+
+module.exports = AiController;
